7288ea9b0da81d6406dd33c53564239977747075
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2025-05-29 21:57:55 -0300

    Tela de anotação v3

--- app/src/main/java/com/example/anotamais/Notes.java
+++ app/src/main/java/com/example/anotamais/Notes.java
@@ -2,6 +2,7 @@ package com.example.anotamais;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.text.Editable;
@@ -18,6 +19,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Notes extends AppCompatActivity {
 
     ImageButton btVoltarNotes, btFlashCard;
@@ -25,7 +29,7 @@ public class Notes extends AppCompatActivity {
     EditText txtTitulo, txtConteudo, txtPergunta, txtResposta;
     String nomeUsuario;
     FrameLayout fundoPopup;
-    LinearLayout criarFlashcard;
+    LinearLayout criarFlashcard, conteudoPrincipal;
     GeminiConfig gemini;
 
     @SuppressLint("ClickableViewAccessibility")
@@ -36,10 +40,10 @@ public class Notes extends AppCompatActivity {
 
         gemini = new GeminiConfig(BuildConfig.GEMINI_API_KEY);
         btVoltarNotes = findViewById(R.id.btVoltarNotes);
-        btSalvarNotes = findViewById(R.id.btSalvarNotes);
-        btCriarPagina = findViewById(R.id.btCriarPagina);
+        //btSalvarNotes = findViewById(R.id.btSalvarNotes);
         btFlashCard = findViewById(R.id.btFlashCard);
         btGerarComIa = findViewById(R.id.btGerarComIa);
+        conteudoPrincipal = findViewById(R.id.conteudoPrincipal);
         btSalvarFlashcard = findViewById(R.id.btSalvarFlashcard);
         txtTitulo = findViewById(R.id.txtTitulo);
         txtConteudo = findViewById(R.id.txtConteudo);
@@ -55,7 +59,7 @@ public class Notes extends AppCompatActivity {
             finish();
         });
 
-        btSalvarNotes.setOnClickListener(v -> {
+        /*btSalvarNotes.setOnClickListener(v -> {
             String titulo = txtTitulo.getText().toString().trim();
             String conteudo = txtConteudo.getText().toString().trim();
 
@@ -75,9 +79,10 @@ public class Notes extends AppCompatActivity {
             finish();
 
             Toast.makeText(this, "Caderno criado com sucesso!", Toast.LENGTH_SHORT).show();
-        });
+        });*/
         btFlashCard.setOnClickListener(v -> {
             fundoPopup.setVisibility(View.VISIBLE);
+            conteudoPrincipal.animate().alpha(0.3f).setDuration(200).start();
         });
 
         btSalvarFlashcard.setOnClickListener(v -> {
@@ -91,6 +96,9 @@ public class Notes extends AppCompatActivity {
             bancoControllerCard.insereDados(txtPergunta.getText().toString(), txtResposta.getText().toString(), 1);
 
             Toast.makeText(this, "Flashcard criada com sucesso!", Toast.LENGTH_LONG).show();
+
+            fundoPopup.setVisibility(View.GONE);
+            conteudoPrincipal.animate().alpha(1f).setDuration(200).start();
         });
 
         btGerarComIa.setOnClickListener(v -> {
@@ -99,8 +107,11 @@ public class Notes extends AppCompatActivity {
             btSalvarFlashcard.setEnabled(false);
             btGerarComIa.setEnabled(false);
 
+            BancoControllerCard bancoControllerCard = new BancoControllerCard(getBaseContext());
+
             String textoConteudo = txtConteudo.getText().toString();
-            String promptPergunta = "Estou te utilizando como api em um projeto, então quero que vc apenas retorne oq eu pedir, sem vc falar nada. Estou gerando um flashcard, onde tem uma pergunta curta e uma resposta curta, nesse prompt quero que vc retorne apenas uma pergunta sobre essa aula: " + textoConteudo;
+
+            String promptPergunta = "Estou te utilizando como api em um projeto, então quero que vc apenas retorne oq eu pedir, sem vc falar nada. Estou gerando um flashcard, onde tem uma pergunta curta e uma resposta curta, nesse prompt quero que vc retorne apenas uma pergunta sobre essa aula (Lembre-se, pergunta curta!! com no máximo 80 caracteres): " + textoConteudo + " (Faça perguntas diferentes dessas, pois já foram criadas: " + consultaTodasAsPerguntasDosFlashcards();
 
             gemini.getResponse(promptPergunta, new GeminiConfig.GeminiCallback() {
                 @Override
@@ -116,7 +127,7 @@ public class Notes extends AppCompatActivity {
                             txtPergunta.setText(questionResponse);
 
                             // Now, make the second API call for the answer
-                            String promptResposta = "Estou te utilizando como api em um projeto, então quero que vc apenas retorne oq eu pedir, sem vc falar nada. Estou gerando um flashcard, onde tem uma pergunta curta e uma resposta curta, nesse prompt quero que vc retorne apenas a resposta sobre essa pergunta: " + questionResponse;
+                            String promptResposta = "Estou te utilizando como api em um projeto, então quero que vc apenas retorne oq eu pedir, sem vc falar nada. Estou gerando um flashcard, onde tem uma pergunta curta e uma resposta curta, nesse prompt quero que vc retorne apenas a resposta sobre essa pergunta (Lembre-se, tem que ser resposta curta com no máximo 130 caracteres!!) : " + questionResponse;
 
                             gemini.getResponse(promptResposta, new GeminiConfig.GeminiCallback() {
                                 @Override
@@ -168,7 +179,7 @@ public class Notes extends AppCompatActivity {
                 criarFlashcard.getGlobalVisibleRect(outRect);
                 if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
                     fundoPopup.setVisibility(View.GONE);
-                    v.performClick();
+                    conteudoPrincipal.animate().alpha(1f).setDuration(200).start();
                 }
             }
             return true;
@@ -182,7 +193,7 @@ public class Notes extends AppCompatActivity {
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 // Conta as quebras de linha
                 int lines = txtPergunta.getLineCount();
-                if (lines > 2) {
+                if (lines > 3) {
                     // Remove o último caractere digitado
                     txtPergunta.setText(s.subSequence(0, s.length() - 1));
                     txtPergunta.setSelection(txtPergunta.getText().length()); // move o cursor pro final
@@ -213,5 +224,47 @@ public class Notes extends AppCompatActivity {
             public void afterTextChanged(Editable s) {
             }
         });
+    }
+    private List<FlashcardModel> consultaTodosFlashcards() {
+        List<FlashcardModel> lista = new LinkedList<FlashcardModel>();
+
+
+        BancoControllerCard bd = new BancoControllerCard(getBaseContext());
+        Cursor dados = bd.carregaFlashcards();
+
+
+        if (dados != null && dados.moveToFirst()) {
+            do {
+                FlashcardModel item = new FlashcardModel();
+                item.setId(dados.getInt(0));
+                item.setPergunta(dados.getString(1));
+                item.setResposta(dados.getString(2));
+                lista.add(item);
+            } while (dados.moveToNext());
+        }else{
+            String msg = "Não há perguntas de flashcard criada";
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        }
+        return lista;
+    }
+    private List<String> consultaTodasAsPerguntasDosFlashcards() {
+        List<String> lista = new LinkedList<String>();
+
+
+        BancoControllerCard bd = new BancoControllerCard(getBaseContext());
+        Cursor dados = bd.carregaFlashcards();
+
+
+        if (dados != null && dados.moveToFirst()) {
+            do {
+                String item;
+                item = dados.getString(1);
+                lista.add(item);
+            } while (dados.moveToNext());
+        }else{
+            String msg = "Não há perguntas de flashcard criada";
+            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        }
+        return lista;
     }
 }
